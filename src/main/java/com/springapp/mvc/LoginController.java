8661496668268e76df5b569b7a69930407c1f58f
roawoo <roawoo@hotmail.com>
2015-11-14 20:13:35 -0500

    Update LoginController.java
    validate info completed
    can either accept id or email for login

--- src/main/java/com/springapp/mvc/LoginController.java
+++ src/main/java/com/springapp/mvc/LoginController.java
@@ -6,13 +6,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.sql.*;
 import java.util.List;
 
 @Controller
 public class LoginController {
 
-    private static final String DEFAULT_EMAIL = "email@email.com";
-    private static final String DEFAULT_PASSWORD = "password";
+   // private static final String DEFAULT_EMAIL = "email@email.com";
+    //private static final String DEFAULT_PASSWORD = "password";
 
     @RequestMapping(method = RequestMethod.GET)
     public String defaultPage(ModelMap model) {
@@ -25,17 +26,29 @@ public class LoginController {
         //TODO Read UserLogin Data from loginData.txt
         List<LoginData> loginDataList;
 
-        String email = loginData.getEmail();
+        String id = loginData.getId();
         String password = loginData.getPassword();
 
-        if (isValidLoginData(email, password)) {
+        if (isValidLoginData(id, password)) {
             return "redirect:/dashBoard";
         }
         return "login";
     }
 
-    private boolean isValidLoginData(String email, String password) {
-        return (email.equals(DEFAULT_EMAIL) && password.equals(DEFAULT_PASSWORD));
+    private boolean isValidLoginData(String identification, String password) throws SQLException{
+        boolean result = false;
+        Statement stmt = DBCon.createStatement();
+        String sql = "SELECT * FROM user WHERE Email = '"+identification+"' AND Password = '"+password+"'";
+        ResultSet rs = stmt.executeQuery(sql);
+        if (rs.next()){
+            result = true;
+        }
+        sql = "SELECT * FROM user WHERE ID = '"+identification+"' AND Password = '"+password+"'";
+        rs = stmt.executeQuery(sql);
+        if (rs.next()){
+            result = true;
+        }
+        return result;
     }
 
     @RequestMapping(value = "/register", method = {RequestMethod.GET, RequestMethod.POST})

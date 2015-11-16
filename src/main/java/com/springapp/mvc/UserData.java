07642e54dd84acdb084440af7fdf04a44acd0f1b
roawoo <roawoo@hotmail.com>
2015-11-16 16:37:06 -0500

    Create UserData.java

---
+++ src/main/java/com/springapp/mvc/UserData.java
@@ -0,0 +1,92 @@
+package com.springapp.mvc;
+
+/**
+ * Created by Neoburi on 15/11/2015.
+ */
+public class UserData {
+    private String fname;
+    private String lname;
+    private String id;
+    private String password;
+    private String address;
+    private String postalcode;
+    private int age;
+    private String gender;
+    private String email;
+    private String city;
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity(){ return city;}
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostalcode() {
+        return postalcode;
+    }
+
+    public void setPostalcode(String postalcode) {
+        this.postalcode = postalcode;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCity(String city) {this.city = city;}
+}
